963df5827d310a21435a4aa1aa1a8893cdbb113d
Sai <sylonzero@gmail.com>
2018-05-09 01:45:20 -0400

    Updating dlog

--- src/dlog.js
+++ src/dlog.js
@@ -28,20 +28,13 @@ function dlog(namespace) {
 }
 
 dlog.namespaces = {
-  module1: {
+  default: {
     active: false,
     style: {
       fontSize: '10pt',
       color: 'gray'
     }
-  },
-  module2: {
-    active: true,
-    style: {
-      fontSize: '10pt',
-      color: 'green'
-    }
-  }
+  }  
 };
 
 dlog.showOnly = [];
