2484f2975aff9d38a685bec309e1013c5d617202
Sai <sylonzero@gmail.com>
2018-05-09 01:46:21 -0400

    Create dlog-config.js

---
+++ samples/dlog-config.js
@@ -0,0 +1,18 @@
+dlog.namespaces = {
+  module1: {
+    active: false,
+    style: {
+      fontSize: '10pt',
+      color: 'gray'
+    }
+  },
+  module2: {
+    active: true,
+    style: {
+      fontSize: '10pt',
+      color: 'green'
+    }
+  }
+};
+
+dlog.showOnly = [];
